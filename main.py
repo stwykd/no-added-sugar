91d049d9d3a41b85e75268d58a347a601e81c965
stwykd <5012027+stwykd@users.noreply.github.com>
2020-02-23 19:50:23 +0100

    load page from html

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.write("hello world!")
+        self.render("front.html")
 
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
