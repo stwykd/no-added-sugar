4ee5de7d0f1eba33427526467864b50635cac178
stwykd <5012027+stwykd@users.noreply.github.com>
2020-02-24 13:43:25 +0100

    add blog post submit page

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import os
 import webapp2
 import jinja2
@@ -6,6 +7,8 @@ from google.appengine.ext import db
 
 templ_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(templ_dir), autoescape=True)
+
+
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -17,6 +20,15 @@ class Handler(webapp2.RequestHandler):
 class MainPage(Handler):
     def get(self):
         self.render("front.html")
+
+    def post(self):
+        title = self.request.get("title")
+        post = self.request.get("message")
+
+        if title and post:
+            self.write("post submitted. yas! 🔥✌️")
+        else:
+            self.render("front.html", error="both a title and a message are required!")
 
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
