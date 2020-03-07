58e5d6f4db073559cbb3d15435f0821bd38d2c08
stwykd <5012027+stwykd@users.noreply.github.com>
2020-03-07 10:28:55 +0100

    Add signup form and welcome page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import os
+import re
 
 import jinja2
 import webapp2
@@ -72,18 +73,76 @@ class PostPage(Handler):
         else:
             self.render("permalink.html", post=post)
 
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+
+class SignupPage(Handler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        have_error = False
+        params = dict(username=username, email=email)
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif password != verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup.html', **params)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+
+class WelcomePage(Handler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username=username)
+        else:
+            self.redirect('/signup')
+
+
 class NotFoundPage(Handler):
     def get(self):
         self.error(404)
         self.render('404.html')
 
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/blog', BlogPage),
-    ('/blog/', BlogPage),
-    ('/blog/submit', SubmitPage),
-    ('/blog/submit/', SubmitPage),
-    ('/blog/([0-9]+)', PostPage),
-    ('/blog/([0-9]+)/', PostPage),
+    ('/signup', SignupPage), ('/signup/', SignupPage),
+    ('/welcome', WelcomePage), ('/welcome/', SignupPage),
+    ('/blog', BlogPage), ('/blog/', BlogPage),
+    ('/blog/submit', SubmitPage), ('/blog/submit/', SubmitPage),
+    ('/blog/([0-9]+)', PostPage), ('/blog/([0-9]+)/', PostPage),
     ('/.*', NotFoundPage),
 ], debug=True)
